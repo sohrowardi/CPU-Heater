bafefa4c45e67fe78e124abc8917a69fe8ca8009
sohrowardi <sohrowardi99@gmail.com>
2024-09-27 11:05:17 +0600

    modified:   cpu_heater.py

--- cpu_heater.py
+++ cpu_heater.py
@@ -1,25 +1,18 @@
 import multiprocessing
 import sys
 
-def intensive_computation():
-    number = 0
+def round_robin_count():
     while True:
+        number = 0
         if number >= sys.maxsize:
             number = 0
         else:
             number += 1
-        # Adding more complex calculations
-        _ = number ** 2
-        _ = number ** 0.5
-        _ = number * number
 
 if __name__ == "__main__":
+    process_count = 1
     print("Heating up the CPU")
-    processes = []
-    for _ in range(multiprocessing.cpu_count()):
-        process = multiprocessing.Process(target=intensive_computation)
+    while process_count <= multiprocessing.cpu_count():
+        process = multiprocessing.Process(target=round_robin_count)
         process.start()
-        processes.append(process)
-
-    for process in processes:
-        process.join()
+        process_count += 1
